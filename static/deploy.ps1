65191c878b454ad8ebf728a5a4749bb2704c7dcc
Meysam Kazemi <kazemii.meysam@gmail.com>
2020-01-20 18:26:02 -0700

    updating deploy script

--- static/deploy.ps1
+++ static/deploy.ps1
@@ -1,11 +1,13 @@
 Write-Host "Starting..."
 git --version
 
+Write-Host ""
 Write-Host "pulling latest changes from the repository..."
 git pull origin master
 
-Write-Host "Completed. Copying files..."
-Copy-Item -Path * -Destination ..\www\ -Exclude deploy.ps1 -Recurse -PassThru
+Write-Host ""
+Write-Host "Completed. Copying files... (Please wait. It will take a few minutes.)"
+Copy-Item -Path * -Destination ..\www\ -Exclude deploy.ps1 -Recurse
 
 Write-Host "Done. The website is now online."
 
