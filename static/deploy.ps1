27f018417736b234f4aa43bba80d53dc32b9cd9f
Meysam Kazemi <kazemii.meysam@gmail.com>
2020-01-20 18:39:18 -0700

    updating deploy script

--- static/deploy.ps1
+++ static/deploy.ps1
@@ -7,8 +7,9 @@ git pull origin master
 
 Write-Host ""
 Write-Host "Completed. Copying files... (Please wait. It will take a few minutes.)"
-Copy-Item -Path * -Destination ..\www\ -Exclude deploy.ps1 -Recurse
+Copy-Item -Path * -Destination ..\www\ -Exclude deploy.ps1 -Recurse -Force -PassThru
 
+Write-Host ""
 Write-Host "Done. The website is now online."
 
 Write-Host "Press any key to continue..."
