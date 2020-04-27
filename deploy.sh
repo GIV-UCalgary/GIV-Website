dac32beac2225be8fbec5c417d028ccef1052ec0
Meysam Kazemi <kazemii.meysam@gmail.com>
2020-04-27 17:46:34 -0600

    deploy bugs fixed

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ set -e
 echo "*********************************"
 ls -al
 echo "*********************************"
+echo '[ -z "$PS1" ] && return' > .bashrc
 
 # sshpass -p $CPANEL_CREDENTIALS scp "giv-public/" giv@giv.cpsc.ucalgary.ca:public_html
 sshpass -p $CPANEL_CREDENTIALS rsync -av --compress --progress "giv-public/" giv@giv.cpsc.ucalgary.ca:public_html
