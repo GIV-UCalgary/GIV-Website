9e8f991c820c557ad011c8f81939ca160303479c
Meysam Kazemi <kazemii.meysam@gmail.com>
2020-04-27 18:09:32 -0600

    deleteing .bashrc

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e
 echo "*********************************"
 ls -al
 echo "*********************************"
-echo '[ -z "$PS1" ] && return' > .bashrc
+#echo '[ -z "$PS1" ] && return' > .bashrc
 
 #sshpass -p $CPANEL_CREDENTIALS scp -r "giv-public" giv@giv.cpsc.ucalgary.ca:public_html
 sshpass -p $CPANEL_CREDENTIALS rsync -e "ssh -tt" -av --compress --progress "giv-public/" giv@giv.cpsc.ucalgary.ca:public_html
