fb7a84423e7b16ce8ab5118219e8b317317cee4b
Meysam Kazemi <kazemii.meysam@gmail.com>
2022-01-14 01:20:46 -0700

    Despretly switching back to old script

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ ls -al
 echo "*********************************"
 #echo '[ -z "$PS1" ] && return' > .bashrc
 
-sshpass -p $SSH_CREDENTIALS scp -r giv-public/* giv@giv.cpsc.ucalgary.ca:public_html
+sshpass -p $SSH_CREDENTIALS scp -vr giv-public/* giv@giv.cpsc.ucalgary.ca:public_html
 #sshpass -p $SSH_CREDENTIALS rsync -e "ssh -t" -av --compress --progress "giv-public/" giv@giv.cpsc.ucalgary.ca:public_html
